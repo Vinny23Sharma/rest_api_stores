05acf791886590b23f0cbec69cda55dd95565c4b
Vinny Sharma <Vinny49990@gmail.com>
2021-06-30 21:49:18 +0530

    Postgress error resolved

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+uri = os.getenv('DATABASE_URL')
+if uri and uri.startswith('postgres://'):
+    uri = uri.replace('postgres://','postgresql://',1)
+else:
+    uri = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']= uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
